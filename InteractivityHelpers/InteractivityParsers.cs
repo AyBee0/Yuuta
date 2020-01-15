d47c5bec15f51cf67a60637fe405edfb8942cff9
Ali Bdeir <ali@bdeir.org>
2020-01-15 22:34:06 +0200

    Fixed mention problem in newevent command

--- InteractivityHelpers/InteractivityParsers.cs
+++ InteractivityHelpers/InteractivityParsers.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace InteractivityHelpers
 {
@@ -12,14 +13,24 @@ namespace InteractivityHelpers
 
         public static List<DiscordRole> ParseSentRoles(this InteractivityResult<DiscordMessage> interactivityResult)
         {
-            if (interactivityResult.Result == null)
+             if (interactivityResult.Result == null)
             {
                 return null;
             }
             var result = interactivityResult.Result;
             var guildRoleNames = result.Channel.Guild.Roles.Values.Select(x => x.Name.Trim().ToLower()).ToList();
             var guildRoles = result.Channel.Guild.Roles.Values;
-            return result.Content
+            //var matches = Regex.Matches(result.Content, @"<@&[0-9]*>");
+            //foreach (var match in matches)
+            //{
+
+            //}
+            string content = Regex.Replace(result.Content, @"<@&[0-9]*>\s*,?", "").Trim().Replace(",,","").Replace(", ,","");
+            foreach (var mention in result.MentionedRoles)
+            {
+                content = content + $",{mention.Name}";
+            }
+            return content
                 .Replace("@", "")
                 .Split(",")
                 .Select(x => x.Trim().ToLower() == "everyone" ? "@everyone" : x.Trim().ToLower())

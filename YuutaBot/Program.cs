daf0c8571dcfbdbad25138cbcc1c217dd4e4c996
TheAb03 <ali@bdeir.org>
2020-05-25 16:36:33 +0300

    Integrated with discord

---
+++ YuutaBot/Program.cs
@@ -0,0 +1,25 @@
+﻿using DSharpPlus;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace YuutaBot
+{
+    class Program
+    {
+        static void Main(string[] args) => MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
+        static async Task MainAsync(string[] args)
+        {
+            _ = args;
+            var discord = new DiscordClient(new DiscordConfiguration
+            {
+                Token = Environment.GetEnvironmentVariable("yuutatoken"),
+                TokenType = TokenType.Bot
+            });
+            var user = await discord.GetCurrentApplicationAsync();
+            Console.WriteLine($"{user.Name} version {Assembly.GetExecutingAssembly().GetName().Version}");
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+    }
+}
